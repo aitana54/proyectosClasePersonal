a3a6c54b43a8a296aaf57a36ea037abe9a4c1d86
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-01-31 10:05:14 +0100

    Inicio variable

--- Objetos/Perro/src/model/Perro.java
+++ Objetos/Perro/src/model/Perro.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Perro {
+    //Inicio variables
+    String nombre;
+    String raza;
+    int edad;
+    float peso;
+    int nivelEnergia;
+    int nivelHambre;
+    //Fin variables
 }
