e8e6c53008ee9225ccd8eefef0e694a93cf5a600
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-01-31 11:28:03 +0100

    Añado constructor2

--- Objetos/Perro/src/model/Perro.java
+++ Objetos/Perro/src/model/Perro.java
@@ -10,6 +10,14 @@ public class Perro {
     private int nivelHambre = 0;
     //Fin variables
 
+    public Perro() {
+        nombre = "Momo";
+        raza = "Huski";
+        edad = 3;
+        peso = 2.1f;
+        nivelEnergia = 100;
+        nivelHambre = 50;
+    }
     public Perro(String nombre, String raza, int edad, float peso, int nivelEnergia, int nivelHambre) {
         this.nombre = nombre;
         this.raza = raza;
@@ -22,7 +30,7 @@ public class Perro {
     /**
      * Función para dormir, aumenta nivel energia
      */
-    public void dormir() {
+    public void dormir(int horas) {
         nivelEnergia += 50;
     }
 
